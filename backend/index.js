4900f793d8c31aad8780264156c51d2ee05bd604
Luis Vallejo <vallejoaguilar@gmail.com>
2020-04-16 23:25:10 -0500

    fixes! 😴 😴 😴

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,22 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-console.log(process.env.NODE_ENV);
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
-const cors = require('cors');
 
-//initializations
+// Initializations
 const app = express();
 require('./database');
 
-const PORT = process.env.NODE_PORT || 3000;
-const ENV = process.env.NODE_ENV || 'Development';
+// settings
+app.set('port', process.env.PORT || 4000);
 
-//settings
-//app.set(`port`, PORT || 3000);
-
-//midlewares ( todos lo midlewares de express son funciones)
+// middlewares
 app.use(morgan('dev'));
-
+app.use(cors());
 const storage = multer.diskStorage({
   destination: path.join(__dirname, 'public/uploads'),
   filename(req, file, cb) {
@@ -31,19 +27,14 @@ const storage = multer.diskStorage({
 app.use(multer({ storage }).single('image'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
-//routes
+
+// routes
 app.use('/api/books', require('./routes/books'));
 
-//static files
+// static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-//start the server
-// app.listen(app.get(`port`), () => {
-//   console.log(`Server on server`, app.get(`port`));
-// });
-
-app.listen(PORT, (err) => {
-  if (err) console.error('❌ Unable to connect the server: ', err);
-  console.log(`🌍 Server listening on port ${PORT} - ${ENV} environment`);
+// start the server
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
 });
